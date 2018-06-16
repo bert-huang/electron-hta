08b46a1cb30d4606c9188332f00a28926774de75
bert-huang <bert.huang@outlook.com>
2018-06-16 13:20:01 +1200

    Changed structure in preparation for packaging from electron-packager. Fixed electron process leaking.

--- index.js
+++ index.js
@@ -1,16 +1,219 @@
+const os = require('os');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
+const findprocess = require('find-process');
 const paths = require('path');
-const cp = require('child_process');
+const { app, BrowserWindow } = require('electron');
+const yargs = require('yargs');
 
-let args = [paths.join(__dirname, 'app', 'main.js')];
-args = args.concat(process.argv.slice(2));
-try {
-  const electron = require('electron');
-  const app = cp.spawn(electron, args, {
-    stdio: 'inherit',
-    detached: true
-  });
-  app.unref();
-}
-catch (e) {
-  process.stderr.write("Cannot launch electron.", e);
-}
+/*
+ * Parses the command line arguments
+ */
+const parseArguments = () => (yargs
+  .options({
+    path: {
+      alias: 'p',
+      describe: 'Path (URL) to launch',
+      type: 'string',
+      demandOption: true,
+    },
+    width: {
+      alias: 'x',
+      describe: 'Width of the window',
+      default: 1024,
+      type: 'number',
+    },
+    height: {
+      alias: 'y',
+      describe: 'Height of the window',
+      default: 768,
+      type: 'number',
+    },
+    singleton: {
+      alias: 's',
+      describe: 'Only allow a single instance',
+      default: false,
+      type: 'boolean',
+      implies: 'singletonId',
+    },
+    singletonId: {
+      alias: 'i',
+      describe: 'Identifier for the singleton option',
+      default: null,
+      type: 'string',
+    },
+    fullscreen: {
+      alias: 'f',
+      describe: 'Launch the window in full screen mode',
+      default: false,
+      type: 'boolean',
+    },
+    alwaysOnTop: {
+      alias: 't',
+      describe: 'Force the window to be always on top of other windows',
+      default: false,
+      type: 'boolean',
+    },
+    showMenu: {
+      alias: 'm',
+      describe: 'Show menu bar in the window',
+      default: false,
+      type: 'boolean',
+    },
+  })
+  .alias('help', 'h')
+  .alias('version', 'v')
+  .parse(process.argv.slice(1)));
+
+/*
+ * Return the accepted URL
+ */
+const getUrl = (url) => {
+  if (url.startsWith('http://') || url.startsWith('https://') || url.startsWith('file://')) {
+    return url;
+  }
+  try {
+    if (fs.statSync(url).isFile()) {
+      return `file://${url}`;
+    }
+  }
+  catch (e) {
+    return null;
+  }
+  return null;
+};
+
+/*
+ * Find the process with the given process ID
+ */
+const findProcess = async (pid) => {
+  const list = await findprocess('pid', pid);
+  return list.length ? list[0] : null;
+};
+
+/**
+ * Forcefully create the directory.
+ * If a non-directory exists, remove it
+ * and create a new directory on top of it.
+ */
+const forceCreateDirectory = (dir) => {
+  if (!fs.existsSync(dir)) {
+    mkdirp.sync(dir);
+  }
+  else if (!fs.statSync(dir).isDirectory()) {
+    rimraf.sync(dir);
+    mkdirp.sync(dir);
+  }
+};
+
+/**
+ * Main routine
+ */
+(async () => {
+  let win = null;
+
+  const argv = parseArguments();
+  /* Extract CLI arguments */
+  const {
+    path,
+    width,
+    height,
+    alwaysOnTop,
+    fullscreen,
+    showMenu,
+    singleton,
+    singletonId,
+  } = argv;
+  const url = getUrl(path);
+  if (!url) {
+    process.stderr.write(`Invalid URL: ${path}`);
+    process.exit(1);
+  }
+
+  const createWindow = (onClose) => {
+    win = new BrowserWindow({
+      width,
+      height,
+      alwaysOnTop,
+      show: false,
+      webPreferences: {
+        nodeIntegration: false,
+        sandbox: true,
+      },
+    });
+    win.loadURL(url);
+    win.once('closed', onClose);
+    win.once('ready-to-show', () => {
+      if (!showMenu) {
+        win.setMenu(null);
+      }
+      win.setFullScreen(fullscreen);
+      win.show();
+    });
+  };
+
+  app.on('window-all-closed', () => {
+    app.quit();
+  })
+
+  /* If singleton mode is on, try obtaining
+   * the lock (a simple lock by file checking). */
+  if (singleton) {
+    /* Always create locks in the OS temp directory. */
+    const tmpDir = paths.join(os.tmpdir(), 'hta-locks');
+    forceCreateDirectory(tmpDir);
+
+    /* Attempt to obtain lock */
+    const htaLock = paths.join(tmpDir, singletonId);
+    if (fs.existsSync(htaLock)) {
+      /* Remove lock if it is not a file */
+      if (!fs.statSync(htaLock).isFile()) {
+        rimraf.sync(htaLock);
+      }
+      /* Read the PID stored in the lock file and attempt
+       * to find an electron process with the same PID.
+       * If a process is indeed found, that means an existing
+       * instance of the singleton is already running. */
+      else {
+        const pid = fs.readFileSync(htaLock, (err) => {
+          if (err) {
+            process.stderr.write(`Unable to read lock: ${singletonId}`);
+            process.exit(1);
+          }
+        });
+        const proc = await findProcess(pid);
+        if (proc.name === 'electron') {
+          process.stderr.write(`Instance already running: ${singletonId}`);
+          process.exit(1);
+        }
+        /* In the scenario where the lock is not cleaned up correctly,
+         * and we cannot find an electron process with the given PID,
+         * remove the lock. */
+        else {
+          rimraf.sync(htaLock);
+        }
+      }
+    }
+    /* Create the lock and write the current PID to the lock file. */
+    fs.writeFileSync(htaLock, process.pid, (err) => {
+      if (err) {
+        process.stderr.write(`Unable to write lock: ${singletonId}`);
+        process.exit(1);
+      }
+    });
+
+    app.on('ready', createWindow.bind(this, () => {
+      win = null;
+      /* Clean up lock file on exit. */
+      rimraf.sync(htaLock);
+    }));
+  }
+
+  /* No special logic for non-singleton launch. */
+  else {
+    app.on('ready', createWindow.bind(this, () => {
+      win = null;
+    }));
+  }
+})();
---
+++ launcher.js
@@ -0,0 +1,16 @@
+const paths = require('path');
+const cp = require('child_process');
+
+let args = [paths.join(__dirname, 'index.js')];
+args = args.concat(process.argv.slice(2));
+try {
+  const electron = require('electron');
+  const app = cp.spawn(electron, args, {
+    stdio: 'inherit',
+    detached: true
+  });
+  app.unref();
+}
+catch (e) {
+  process.stderr.write("Cannot launch electron.", e);
+}
